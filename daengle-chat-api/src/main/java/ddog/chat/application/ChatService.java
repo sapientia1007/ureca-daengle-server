ee0dca1420a1450b01226f5ed897c2bd75b1229d
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2025-03-11 20:28:50 +0900

    [refactroing-8] 채팅 서비스단 수정

--- daengle-chat-api/src/main/java/ddog/chat/application/ChatService.java
+++ daengle-chat-api/src/main/java/ddog/chat/application/ChatService.java
@@ -38,7 +38,6 @@ public class ChatService {
     private final AccountPersist accountPersist;
 
     public ChatMessagesListResp getAllMessagesByRoomId(Role role, Long userAccountId, Long otherUserId) {
-        System.out.println("체크");
         Long chatRoomId ;
         String otherUserProfile = null;
         String otherUserName = null;
